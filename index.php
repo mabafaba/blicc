e41a126ae53729b6fc8cfba52d107e64e059e2b3
mabafaba <barner.mar@gmail.com>
2017-02-21 15:52:43 +0100

    erster uplode

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<?php $name = "martin" ;?>
+
+<html>
+<head>
+	<title><?php echo($name) ?></title>
+</head>
+<body>
+<?php for ($i=0; $i < 10; $i++) { ?>
+	<br><div style="color: red">yeah</div>
+	<?php
+	echo $name;
+} ?>
+
+</body>
+</html>
